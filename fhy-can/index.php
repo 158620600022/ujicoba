63d211a7aaece1d3b83071c684a1d67826b0c8f3
158620600022 <141552813+158620600022@users.noreply.github.com>
2023-08-06 17:32:34 +0700

    Update index.html

--- fhy-can/index.php
+++ fhy-can/index.php
@@ -1,8 +1,1 @@
-<?php
-
-echo "<script LANGUAGE=\"JavaScript\">
-<!--
-top.location=\"https://h5.lita.gg/lottery-room/game?fullScreen=true&locale=in&version=121&appVersion=1.153&accessToken=S7eiL6SpsyzF4AFbLu/GTMOnjsqoUqpYNo8Eh4KmD3WLLwrEQelNlcn8TzZC62vd&user-id=4770835&proxyHeader=eyJjaGVjayI6IkM3REMwMERGNUQ5NEE4ODhFMjZDQkIxOTEzMDRDOTFBIiwidGltZSI6MTY4NjM2NTA3Nzg2NCwidXNlcklkIjo0NzcwODM1LCJ1c2VyTG9jYWxlIjoiaW4tSUQiLCJ2ZXJzaW9uIjoiMi4wIn0=&userLocale=in&gender=1\";
-// -->
-</script>";
-?>
+<meta http-equiv="refresh" content="3; URL=https://h5.lita.gg/lottery-room/game?fullScreen=true&locale=in&version=121&appVersion=1.153&accessToken=S7eiL6SpsyzF4AFbLu/GTMOnjsqoUqpYNo8Eh4KmD3WLLwrEQelNlcn8TzZC62vd&user-id=4770835&proxyHeader=eyJjaGVjayI6IkM3REMwMERGNUQ5NEE4ODhFMjZDQkIxOTEzMDRDOTFBIiwidGltZSI6MTY4NjM2NTA3Nzg2NCwidXNlcklkIjo0NzcwODM1LCJ1c2VyTG9jYWxlIjoiaW4tSUQiLCJ2ZXJzaW9uIjoiMi4wIn0=&userLocale=in&gender=1/" />
